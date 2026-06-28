33c97590a29bbfba15904958fa657b600387cb25
sadia <sadiaazim602@gmail.com>
2026-06-28 11:18:41 +0530

    Add elements for duplicate

--- src/com/practice/streamapi/FindDuplicate.java
+++ src/com/practice/streamapi/FindDuplicate.java
@@ -2,7 +2,7 @@ package com.practice.streamapi;
 import java.util.*;
 public class FindDuplicate {
     public static void  main(String[] args){
-        List<Integer> list = Arrays.asList(1,2,2,3,4,4,5,6,7);
+        List<Integer> list = Arrays.asList(1,2,2,3,4,4,5,6,7,10,2);
         Set<Integer> set = new HashSet<>();
         list.stream().filter(n -> !set.add(n)).forEach(System.out::println);
 

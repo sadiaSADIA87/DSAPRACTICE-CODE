4eb786ee10f501571113c3437f06914b41efe56d
sadia <sadiaazim602@gmail.com>
2026-07-01 13:18:23 +0530

    added to matrx

--- src/com/practice/matrix/RotateMatrix.java
+++ src/com/practice/matrix/RotateMatrix.java
@@ -1,4 +1,57 @@
 package com.practice.matrix;
 
 public class RotateMatrix {
-}
+
+        public static void main(String[] args) {
+
+            int[][] matrix = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+
+            int n = matrix.length;
+
+            // Step 1 : Transpose
+            for (int i = 0; i < n; i++) {
+
+                for (int j = i + 1; j < n; j++) {
+
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+
+                }
+            }
+
+            // Step 2 : Reverse every row
+            for (int i = 0; i < n; i++) {
+
+                int left = 0;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    int temp = matrix[i][left];
+                    matrix[i][left] = matrix[i][right];
+                    matrix[i][right] = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+
+            // Print Matrix
+            for (int i = 0; i < n; i++) {
+
+                for (int j = 0; j < n; j++) {
+
+                    System.out.print(matrix[i][j] + " ");
+
+                }
+
+                System.out.println();
+            }
+
+        }
+    }
